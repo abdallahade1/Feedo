11eaa8846b9bd9d7b49850949f315c2bf3602ba5
abdallahade1 <abdoadelshabaan@gmail.com>
2024-12-25 01:58:17 +0200

    front

--- food-delivery-platform/client/src/App.js
+++ food-delivery-platform/client/src/App.js
@@ -3,7 +3,7 @@
 import React, { useContext } from "react";
 import RestaurantList from "./components/RestaurantList";
 import DishesMenu from "./components/DishesMenu";
-import Cart from "./components/Cart";
+import Cart from "./components/cart";
 import { RestaurantContext } from "./contexts/RestaurantContext";
 import "./App.css"; // Import the CSS file
 
@@ -13,7 +13,7 @@ const App = () => {
     return (
         <>
             <div className="container">
-                <h1 className="header">GFG Food Delivery App</h1>
+                <h1 className="header">Food Delivery platform</h1>
                 <Cart
                     style={{ position: "absolute", right: "20px", top: "20px" }}
                 />

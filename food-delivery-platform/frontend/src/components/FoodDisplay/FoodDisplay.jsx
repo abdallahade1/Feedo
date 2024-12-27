4a458754385e8c595b7f9e80d4d288f89e7bbea2
ShahdAmmar <shahdammar413@gmail.com>
2024-12-27 16:52:42 +0200

    filtering menus

--- food-delivery-platform/frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ food-delivery-platform/frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+ import React, {useContext} from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
@@ -9,8 +9,11 @@ const FoodDisplay = ({category}) => {
     <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
-            {food_list.map((item, index)=>{
+          {food_list.map((item, index)=>{
+              if (category==='All' || category===item.category)
+              {
                 return <FoodItem key = {index} id = {item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+              }
             })}
         </div>
     </div>

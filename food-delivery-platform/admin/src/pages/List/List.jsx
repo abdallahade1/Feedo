cc0420ad90009d9a2415d880910168fd6086d851
ShahdAmmar <shahdammar413@gmail.com>
2024-12-28 23:01:13 +0200

    Add page

---
+++ food-delivery-platform/admin/src/pages/List/List.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import './List.css'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const List = () => {
+
+  return (
+    <div>
+      list
+    </div>
+  )
+}
+
+export default List

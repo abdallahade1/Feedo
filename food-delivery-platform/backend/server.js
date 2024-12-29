c2400eb39b535704dbb53dd79e569e05f1ca638c
ShahdAmmar <shahdammar413@gmail.com>
2024-12-29 13:06:56 +0200

    order placement & payment

--- food-delivery-platform/backend/server.js
+++ food-delivery-platform/backend/server.js
@@ -5,6 +5,9 @@ import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config'
 import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
+
+
 // app config
 const app = express()
 const port = 4000
@@ -21,6 +24,7 @@ app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads"))
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 
 app.get('/', (req, res) => {

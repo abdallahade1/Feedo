f0d43bfae86842770f209048a113e35e3dbeb512
Yasmeen55098 <133390389+Yasmeen55098@users.noreply.github.com>
2024-12-29 02:00:57 +0200

    styling the'List Items' page

--- food-delivery-platform/admin/src/pages/List/List.jsx
+++ food-delivery-platform/admin/src/pages/List/List.jsx
@@ -3,13 +3,13 @@ import './List.css'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
-const List = () => {
-  const url="http://localhost:4000"
+const List = ({url}) => {
+  
   const [list,setList]= useState([]);
   
   const fetchList= async () =>{
     const response = await axios.get(`${url}/api/food/list`);
-    console.log(response.data);
+    
     if (response.data.success){
       setList(response.data.data);
     }
@@ -18,6 +18,18 @@ const List = () => {
       toast.error("Error")
     }
   }
+
+    const removeFood= async(foodId)=>{
+      const response = await axios.post(`${url}/api/food/remove`,{id:foodId});
+      await fetchList();
+      if(response.data.success){
+        toast.success(response.data.message)
+      }
+      else{
+        toast.error("Error");
+      }
+    }
+
   useEffect(() =>{
     fetchList();
   },[])
@@ -40,7 +52,7 @@ const List = () => {
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>${item.price}</p>
-              <p>X</p>
+              <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
             </div>
           )
         })}

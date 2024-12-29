952f7aca2c37ed61e08e92ebd6d8b890ef2a2f73
abdallahade1 <abdoadelshabaan@gmail.com>
2024-12-29 17:53:05 +0200

    admin panel

--- food-delivery-platform/admin/src/components/Navbar/Navbar.jsx
+++ food-delivery-platform/admin/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
-      <img src={assets.profile_image} alt="" className="profile" />
+      <img src={assets.meeee} alt="" className="profile" />
     </div>
   )
 }
